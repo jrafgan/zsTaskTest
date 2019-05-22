2e53097a86841349f913331a730a30298c73e07f
Maksat Duishenaliev <maksatovy@gmail.com>
2019-05-22 21:43:41 +0600

    some extra variables deleted

--- task2.js
+++ task2.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 
-
 fs.readFile('input.txt', 'utf8', (e, data) => {
 
     let arr = data.toString();
     arr = arr.split('\n');
     arr.map(line => {
-        const somePattern = new RegExp(line, 'gim');
 
         fs.readFile('pattern.txt', 'utf8', (e, data) => {
 
--- task3.js
+++ task3.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 
-
 fs.readFile('input.txt', 'utf8', (e, data) => {
 
     let arr = data.toString();
     arr = arr.split('\n');
     arr.map(line => {
-        const somePattern = new RegExp(line, 'gim');
 
         fs.readFile('pattern.txt', 'utf8', (e, data) => {
 
@@ -14,8 +12,6 @@ fs.readFile('input.txt', 'utf8', (e, data) => {
             let text = data.toString();
             const textArr = text.split('\n');
             textArr.map(string => {
-                // console.log(line.charAt(0), line.slice(-1));
-                // console.log(string.charAt(0), string.slice(-1));
 
                 if (line.charAt(0) === string.charAt(0) && line.slice(-1) === string.slice(-1)) {
                     console.log(line)
